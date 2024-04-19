927420a0e95d1091cbd09653ca8b6637d0608e99
hoanganh27042001 <Anh.nh194417@sis.hust.edu.vn>
2024-04-19 18:26:21 +0700

    update 19/4

--- demo_agent_langgraph.py
+++ demo_agent_langgraph.py
@@ -1,36 +1,20 @@
 import getpass
 import os
 import uuid
-
-
-def _set_if_undefined(var: str):
-    if not os.environ.get(var):
-        os.environ[var] = getpass(f"Please provide your {var}")
-
-
-_set_if_undefined("OPENAI_API_KEY")
-_set_if_undefined("LANGCHAIN_API_KEY")
-_set_if_undefined("TAVILY_API_KEY")
-
-# Optional, add tracing in LangSmith.
-# This will help you visualize and debug the control flow
-os.environ["LANGCHAIN_TRACING_V2"] = "true"
-os.environ["LANGCHAIN_PROJECT"] = "Demo Agent"
+import json
+from typing import Callable, Dict, List, Optional, Union, Annotated, Sequence, TypedDict
+from pathlib import Path
+from pprint import pprint
+import operator
+import functools
+import operator
 
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-# from langchain_community.document_loaders import JSONLoader
+from langchain_community.document_loaders import CSVLoader, PyPDFLoader
 from langchain.docstore.document import Document
 from langchain.document_loaders.base import BaseLoader
 from langchain_community.vectorstores import Chroma
 from langchain_openai import OpenAIEmbeddings
-import json
-from typing import Callable, Dict, List, Optional, Union
-from pathlib import Path
-from pprint import pprint
-
-import operator
-from typing import Annotated, Sequence, TypedDict
-
 from langchain import hub
 from langchain.output_parsers import PydanticOutputParser
 from langchain.prompts import PromptTemplate
@@ -42,10 +26,51 @@ from langchain_core.pydantic_v1 import BaseModel, Field
 from langchain_openai import ChatOpenAI
 from langgraph.prebuilt import ToolInvocation
 from langchain_core.output_parsers import StrOutputParser
+from langchain.tools.retriever import create_retriever_tool
+from langgraph.graph import END, StateGraph
+
+from langgraph.prebuilt import ToolExecutor
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_core.messages import BaseMessage, HumanMessage
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.output_parsers.openai_functions import JsonOutputFunctionsParser
+
+
+
+def _set_if_undefined(var: str):
+    if not os.environ.get(var):
+        os.environ[var] = getpass.getpass(f"Please provide your {var}")
+
+
+_set_if_undefined("OPENAI_API_KEY")
+_set_if_undefined("LANGCHAIN_API_KEY")
+_set_if_undefined("TAVILY_API_KEY")
+
+# Optional, add tracing in LangSmith.
+# This will help you visualize and debug the control flow
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_PROJECT"] = "Demo Agent"
 
 
 file_path = "../docs/intent_trade_v2.json"
+file_path2 = "../docs/whales_market_v2.json"
+file_price = "price.pdf"
 # data = json.loads(Path(file_path).read_text())
+"""Load csv file"""
+loader = PyPDFLoader(file_path = file_price)
+data_price = loader.load_and_split()
+
+vectorstore_price = Chroma.from_documents(
+    documents=data_price,
+    collection_name="price",
+    embedding=OpenAIEmbeddings(),
+)
+retrieve_price = vectorstore_price.as_retriever(search_kwargs={"k": 1})
+tool_price = create_retriever_tool(
+    retrieve_price,
+    "retrieve_price_history",
+    "Search and return information about price history of some blockchain token."
+)
 
 """Loader that loads data from JSON."""
 class JSONLoader(BaseLoader):
@@ -71,120 +96,59 @@ loader = JSONLoader(file_path=file_path)
 data = loader.load()
 vectorstore = Chroma.from_documents(
     documents=data,
+    collection_name="data_offline",
     embedding=OpenAIEmbeddings(),
 )
-retriever = vectorstore.as_retriever()
+retrieve_offline_info = vectorstore.as_retriever()
 
-# question = "what is intend trade"
-# docs = retriever.get_relevant_documents(question)
-# docs
+loader1 = JSONLoader(file_path=file_path2)
+data1 = loader1.load()
+vectorstore1 = Chroma.from_documents(
+    documents=data1,
+    collection_name="data_online",
+    embedding=OpenAIEmbeddings(),
+)
+retrieve_online_info = vectorstore1.as_retriever()
 
-from langchain.tools.retriever import create_retriever_tool
 
-tool = create_retriever_tool(
-    retriever, 
-    "retrieve_docs",
+tool_offline = create_retriever_tool(
+    retrieve_offline_info, 
+    "retrieve_offline_docs",
     "Search and return information from the internal source about some blockchain knowledge."
 )
-tools = [tool]
-from langgraph.prebuilt import ToolExecutor
+tool_online = create_retriever_tool(
+    retrieve_online_info, 
+    "retrieve_online_docs",
+    "Search and return information from the online source about some blockchain knowledge."
+)
+tools = [tool_offline, tool_online, tool_price]
 tool_executor = ToolExecutor(tools)
 
-import operator
-from typing import Annotated, Sequence, TypedDict
-from langchain_core.messages import BaseMessage, HumanMessage
+tavily_tool = TavilySearchResults(max_results=2)
+tool_search_executor = ToolExecutor([tavily_tool])
+
 
 class AgentState(TypedDict):
     messages: Annotated[Sequence[BaseMessage], operator.add]
+    next: str
     
 # Edges
-def should_retrieve(state):
-    """
-    Decide whether the agent should retrieve more information or end the process.
-    
-    This function checks the last message in the state for a function call. If a function call is
-    present, the process continues to retrieve information. Otherwise, it ends the process.
-
-    Args:
-        state (messages): The current state
-        
-    Returns:
-        str: A decision to either "continue" the retrieval process or "end" it
-    """
-    print("---DECIDE TO RETRIEVE---")
-    messages = state["messages"]
-    last_message = messages[-1]
-    
-    # If there is no function call, then we finish
-    if "function_call" not in last_message.additional_kwargs:
-        print("---DECISION: DO NOT RETRIEVE / DONE---")
-        return "end"
-    # Otherwise there is a function call, so we continue
-    else:
-        print("---DECISION: RETRIEVE---")
-        return "continue"
-    
-def grade_documents(state):
-    """
-    Determines whether the retrieved documents are relevant to the question.
-
-    Args:
-        state (messages): The current state
-        
-    Returns:
-        str: A decision for whether the documents are relevant or not
-    """
-    print("---CHECK RELEVANCE---")
-    
-    # Data model
-    class grade(BaseModel):
-        """Binary score for relevance check."""
-        binary_score: str = Field(description="Relevance score 'yes' or 'no'")
-        
-    # LLM
-    model = ChatOpenAI(temperature=0, model="gpt-3.5-turbo", streaming=True)
-    # Tool
-    grade_tool_oai = convert_to_openai_tool(grade)
-    
-    # LLM with tool and enforce invocation
-    llm_with_tool = model.bind(
-        tools=[convert_to_openai_tool(grade_tool_oai)],
-        tool_choice={"type": "function", "function": {"name": "grade"}},
+def create_agent(llm, tools, system_message: str):
+    """Create an agent."""
+    functions = [format_tool_to_openai_function(t) for t in tools]
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                system_message,
+            ),
+            MessagesPlaceholder(variable_name="messages"),
+        ]
     )
-    # Parser
-    parser_tool = PydanticToolsParser(tools=[grade])
-    # Prompt
-    prompt = PromptTemplate(
-        template="""You are a grader accessing relevance of a retrieved document to a user question. \n
-        Here is the retrieved document: \n\n {context} \n\n
-        Here is the user question: {question} \n
-        If the document contains keyword(s) or semantic meaning related to the user question, grade it as relevant. \n
-        Give a binary score 'yes' or 'no' score to indicate whether the document is relevant to the question.""",
-        input_variables=["context", "question"],
-    )
-    # Chain
-    chain = prompt | llm_with_tool | parser_tool
-    
-    messages = state["messages"]
-    last_message = messages[-1]
-    question = messages[0].content
-    docs = last_message.content
-    
-    score = chain.invoke(
-        {"question": question,
-         "context": docs}
-    )
-    
-    if grade == "yes":
-        print("---DECISION: DOCS RELEVANT---")
-        return "yes"
-    
-    else:
-        print("---DECISION: DOCS NOT RELEVANT---")
-        print(grade)
-        return "no"
+    return prompt | llm.bind_functions(functions)
     
 ### Nodes
+    
 def agent(state):
     """
     Invokes the agent model to generate a response based on the current state. Given
@@ -205,16 +169,64 @@ def agent(state):
     
     return {"messages": [response]}
 
-def retrieve(state):
-    """
-    Use tool to execute retrieval.
+def agent_node(state, agent, name):
+    response = agent.invoke(state)
+    return {
+        "messages": [response],
+        "next": name,
+    }
+def create_team_supervisor(llm: ChatOpenAI, system_prompt, members) -> str:
+    """An LLM-based router."""
+    options = ["FINISH"] + members
+    function_def = {
+        "name": "route",
+        "description": "Select the next role.",
+        "parameters": {
+            "title": "routeSchema",
+            "type": "object",
+            "properties": {
+                "next": {
+                    "title": "Next",
+                    "anyOf": [
+                        {"enum": options},
+                    ],
+                },
+            },
+            "required": ["next"],
+        },
+    }
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", system_prompt),
+            MessagesPlaceholder(variable_name="messages"),
+            (
+                "system",
+                "Given the conversation above, who should act next?"
+                " Or should we FINISH? Select one of: {options}",
+            ),
+        ]
+    ).partial(options=str(options), team_members=", ".join(members))
+    return (
+        prompt
+        | llm.bind_functions(functions=[function_def], function_call="route")
+        | JsonOutputFunctionsParser()
+    )
+
+def tool_search(state):
+    print("---EXECUTE SEARCHING---")
+    messages = state["messages"]
+    last_message = messages[-1]
     
-    Args:
-        state (messages): the current state
-        
-    Returns:
-        dict: The updated state with retrieved docs
-    """
+    action = ToolInvocation(
+        tool=last_message.additional_kwargs["function_call"]["name"],
+        tool_input=json.loads(
+            last_message.additional_kwargs["function_call"]["arguments"]
+        ),
+    )
+    response = tool_search_executor.invoke(action)
+    function_message = FunctionMessage(content=str(response), name=action.tool)
+    return {"messages": [function_message]}
+def tool_rag(state):
     print("---EXECUTE RETRIEVAL---")
     messages = state["messages"]
     
@@ -229,63 +241,14 @@ def retrieve(state):
     response = tool_executor.invoke(action)
     function_message = FunctionMessage(content=str(response), name=action.tool)
     
-    return {"messages": [function_message]}
-
-def rewrite(state):
-    """
-    Transform the query to produce a better question.
-    
-    Args:
-        state (messages): The current state
-        
-    Returns:
-        dict: The updated state with re-phased question
-    """
-    
-    print("---TRANSFORM QUERY---")
-    messages = state["messages"]
-    question = messages[0].content
-    
-    msg = [HumanMessage(
-        content=f""" \n
-        Look at the input and try to reason about the underlying semantic intent / meaning. \n
-        Here is the initial question:
-        \n ------- \n
-        {question}
-        \n ------- \n
-        Formulate an improved question: """,
-    )]
-    
-    # Grader 
-    model = ChatOpenAI(temperature=0, model="gpt-3.5-turbo", streaming=True)
-    response = model.invoke(msg)
-    return {"message": [response]}
-
-def generate(state):
-    """
-    Generate answer
-    
-    Args:
-        state (messages): The current state
-        
-    Returns:
-        dict: The updated state with re-phrased question
-    """
     print("---GENERATE---")
-    messages = state["message"]
     question = messages[0].content
-    last_message = messages[-1]
     
-    question = messages[0].content
-    docs = last_message.content
+    docs = function_message.content
     
     # Prompt
     prompt = hub.pull("rlm/rag-prompt")
     llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, streaming=True)
-    
-    # Post-processing
-    def format_docs(docs):
-        return "\n\n".join(doc.page_content for doc in docs)
     
     # Chain
     rag_chain = prompt | llm | StrOutputParser()
@@ -294,50 +257,87 @@ def generate(state):
     response = rag_chain.invoke({"context": docs, "question": question})
     return {"messages": [response]}
 
+def router(state):
+    messages = state["messages"]
+    last_message = messages[-1]
+    
+    if "function_call" in last_message.additional_kwargs:
+        if last_message.additional_kwargs["function_call"]["name"] == "tavily_search_results_json":
+            return "search_tool"
+        else:
+            return "call_tool"
+    return "end"
 # Graph
+llm = ChatOpenAI(model="gpt-3.5-turbo")
 
-from langgraph.graph import END, StateGraph
+docs_agent = create_agent(
+    llm,
+    [tool_offline, tool_online],
+    system_message="You should provide information about some blockchain knowledge."
+)
+docs_node = functools.partial(agent_node, agent=docs_agent, name="DocAgent")
+price_agent = create_agent(
+    llm,
+    [tavily_tool, tool_price],
+    system_message="Use tavily_tool if got `Search` keyword in the question. Otherwise, you should retrieve or search for price of any blockchain token."
+)
+price_node = functools.partial(agent_node, agent=price_agent, name="PriceAgent")
 
+supervisor_agent = create_team_supervisor(
+    llm,
+    """You are a supervisor who distribute task between the 
+     following members: DocAgent, PriceAgent. Given the following user request,
+     respond with the member to act next. Each member will perform a
+     task and respond with their results and status. When finished,
+     respond with FINISH.""",
+     ["DocAgent", "PriceAgent"],
+)
 workflow = StateGraph(AgentState)
 
-workflow.add_node("agent", agent)
-workflow.add_node("retrieve", retrieve)
-workflow.add_node("rewrite", rewrite)
-workflow.add_node("generate", generate)
+workflow.add_node("supervisor", supervisor_agent)
+# DocAgent
+workflow.add_node("DocAgent", docs_node)
+workflow.add_node("call_tool", tool_rag)
 
-workflow.set_entry_point("agent")
+# PriceAgent
+workflow.add_node("PriceAgent", price_node)
+workflow.add_node("search_tool", tool_search)
 
+workflow.set_entry_point("supervisor")
+members = ["DocAgent", "PriceAgent"]
+workflow.add_edge("DocAgent", "supervisor")
+workflow.add_edge("PriceAgent", "supervisor")
+conditional_map = {k: k for k in members}
+conditional_map["FINISH"] = END
+workflow.add_conditional_edges("supervisor", lambda x: x["next"], conditional_map)
 workflow.add_conditional_edges(
-    "agent",
-    should_retrieve,
-    {
-        "continue": "retrieve",
-        "end": END,
-    }
+    "PriceAgent",
+    router,
+    {"call_tool": "call_tool", "search_tool": "search_tool", "end": END}
 )
-
 workflow.add_conditional_edges(
-    "retrieve",
-    grade_documents,
-    {
-        "yes": "generate",
-        "no": "rewrite",
-    },
+    "DocAgent",
+    router,
+    {"call_tool": "call_tool", "end": END}
 )
-
-workflow.add_edge("generate", END)
-workflow.add_edge("rewrite", "agent")
+workflow.add_edge("search_tool", END)
+workflow.add_edge("call_tool", END)
 
 app = workflow.compile()
 
+# mess = "What is the price of Whale Markets?"
+# mess = "Search for the current Whale Markets price?"
+# mess = "What is intend trade (offline)?"
+mess = "What is the referal program of Whale Markets?"
 inputs = {
     "messages": [
         HumanMessage(
-            content="What is intend trade?"
+            content=mess
         )
     ]
 }
 for output in app.stream(inputs):
+    # print("output: ",output)
     for key, value in output.items():
         pprint(f"Output from node '{key}':")
         pprint("---")
